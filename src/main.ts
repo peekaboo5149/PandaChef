02c693b7b129d2d7134d9883dc98983490a75f8f
peekaboo5149 <ktanmay5149@gmail.com>
2021-09-27 16:02:57 +0530

    Added models

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ dotenv.config()
 import DatabaseConfiguration from './config/db'
 import serve from './config/server'
 
+//*Load variables from enviorment file
 const port: string = process.env.PORT || '4000'
 const url = <string>(
   process.env.DB_URL?.replace('<password>', <string>process.env.DB_PASSWORD)
 )
 
+//*Check basic workings
 if (typeof url != 'string') {
   console.log(`UNDEFIEND URL`)
   process.exit(1)
 }
 
+//*Start Database
 DatabaseConfiguration.getInstance().config(url)
 
+//*Start server
 serve(port)
